e5c0b3a2ef2d72516c4aee58b31d00df7a883def
NightcoreSergeant <tomazic.tadej@gmail.com>
2018-10-09 20:02:58 +0200

    small optimization

--- kiwi_airports.py
+++ kiwi_airports.py
@@ -17,36 +17,36 @@ city=[]
 airport=[]
 iata=[]
 location=[]
-for i in data['locations']:
-    city.append(i['city']['name'])
-    airport.append(i['name'])
-    iata.append(i['code'])
-    location.append([[i['location']['lat'],i['location']['lon']]])
-
-if len(argv) == 1:
-    for i in range(len(city)):
-        print(airport[i],city[i],iata[i],location[i])
+if '--help' in argv:
+    print(halp)
 else:
-    for command in range(1, len(argv)):
-        if command == '--help':
-            print(halp)    
-        elif command == '--cities':
-            for i in range(len(city)):
-                print(city[i],airport[i])      
-        elif command == '--coords':
-            for i in range(len(city)):
-                print(location[i][0],location[i][1])   
-        elif command == 'iata':
-            for i in range(len(city)):
-                print(iata[i])      
-        elif command == 'names':
-            for i in range(len(city)):
-                print(airport[i])        
-        elif command == '--full':
-            print(data['locations'])
-            
+    for i in data['locations']:
+        city.append(i['city']['name'])
+        airport.append(i['name'])
+        iata.append(i['code'])
+        location.append([[i['location']['lat'],i['location']['lon']]])
+
+    if len(argv) == 1:
+        for i in range(len(city)):
+            print(airport[i],city[i],iata[i],location[i])
+    else:
+        for command in range(1, len(argv)): 
+            if command == '--cities':
+                for i in range(len(city)):
+                    print(city[i],airport[i])      
+            elif command == '--coords':
+                for i in range(len(city)):
+                    print(location[i][0],location[i][1])   
+            elif command == 'iata':
+                for i in range(len(city)):
+                    print(iata[i])      
+            elif command == 'names':
+                for i in range(len(city)):
+                    print(airport[i])        
+            elif command == '--full':
+                print(data['locations'])
 
 
 
 
-    
+
