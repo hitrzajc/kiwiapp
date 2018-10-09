2fad59fc02e7be02bab78d38124cb9988cd57b66
NightcoreSergeant <tomazic.tadej@gmail.com>
2018-10-09 22:12:03 +0200

    wierd position

--- kiwi_airports.py
+++ kiwi_airports.py
@@ -13,40 +13,29 @@ api = ('https://api.skypicker.com/locations?type=subentity&term=GB'
 '&locale=en-US&location_types=airport&limit=60&sort=name')
 
 data = json.loads(requests.get(api).content)
-
-city = []
-airport = []
-iata = []
-lat = []
-lon = []
+position = -1
 if '--help' in argv:
     print(_help)
 else:
     for location in data['locations']:
-        city.append(location['city']['name'])
-        airport.append(location['name'])
-        iata.append(location['code'])
-        lon.append(location['location']['lon'])
-        lat.append(location['location']['lat'])
+        position += 1
+        city = location['city']['name']
+        airport = location['name']
+        iata = location['code']
+        lon = location['location']['lon']
+        lat = location['location']['lat']
     
-    if len(argv) == 1:
-        for i in range(len(city)):
-            print(airport[i], city[i], iata[i], lat[i], lon[i])
-    
-    for command in argv[1:]:
-        if command == '--cities':
-            for i in range(len(city)):
-                print(city[i], airport[i])
-        elif command == '--coords':
-            for i in range(len(city)):
-                print(lat[i], lon[i])
-        elif command == 'iata':
-            for i in range(len(city)):
-                print(iata[i])
-        elif command == 'names':
-            for i in range(len(city)):
-                print(airport[i])
-        elif command == '--full':
-            for location in range(len(city)):
-                for key, val in data['locations'][location].items():
-                   print(key, '=', val)
+        for command in argv[1:]:
+            if command == '--cities':
+                print(city, airport)
+            elif command == '--coords':
+                print(lat, lon)
+            elif command == 'iata':
+                print(iata)
+            elif command == 'names':
+                print(airport)
+            elif command == '--full':
+                for key, val in data['locations'][position].items():
+                    print(key, '=', val)
+        else:
+            print(airport, city, iata, lat, lon)
