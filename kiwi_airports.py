f184982cbf105e5e4174a0e65f5d26c841c814e6
NightcoreSergeant <tomazic.tadej@gmail.com>
2018-10-09 21:13:39 +0200

    finished project

--- kiwi_airports.py
+++ kiwi_airports.py
@@ -1,7 +1,6 @@
 from sys import argv
 import requests
 import json
-
 _help = '''type in any of the fallowing command:
             --help - print help message
             --cities - cities with airports
@@ -10,39 +9,41 @@ _help = '''type in any of the fallowing command:
             --names - name of the airport
             --full - print every detail from each airport
 '''
-api = '''https://api.skypicker.com/locations?type=subentity
-&term=GB&locale=en-US&location_types=airport&limit=60&sort=name'''
+api = 'https://api.skypicker.com/locations?type=subentity&term=GB&locale=en-US&location_types=airport&limit=60&sort=name'
 data = json.loads(requests.get(api).content)
 
 city = []
 airport = []
 iata = []
-location = []
+lat = []
+lon = []
 if '--help' in argv:
     print(_help)
 else:
-    for i in data['locations']:
-        city.append(i['city']['name'])
-        airport.append(i['name'])
-        iata.append(i['code'])
-        location.append([[i['location']['lat'], i['location']['lon']]])
-
-    if len(argv) == 1:
+    for location in data['locations']:
+        city.append(location['city']['name'])
+        airport.append(location['name'])
+        iata.append(location['code'])
+        lon.append(location['location']['lot'])
+        lat.append(location['location']['lat'])
+    
+    for command in argv[1:]:
+        if command == '--cities':
+            for i in range(len(city)):
+                print(city[i], airport[i])
+        elif command == '--coords':
+            for i in range(len(city)):
+                print(lat[i], lon[i])
+        elif command == 'iata':
+            for i in range(len(city)):
+                print(iata[i])
+        elif command == 'names':
+            for i in range(len(city)):
+                print(airport[i])
+        elif command == '--full':
+            for location in data['locations']:
+                for j in location:
+                   print(j, '=', location[j])
+    else:
         for i in range(len(city)):
             print(airport[i], city[i], iata[i], location[i])
-    else:
-        for command in range(1, len(argv)):
-            if command == '--cities':
-                for i in range(len(city)):
-                    print(city[i], airport[i])
-            elif command == '--coords':
-                for i in range(len(city)):
-                    print(location[i][0], location[i][1])
-            elif command == 'iata':
-                for i in range(len(city)):
-                    print(iata[i])
-            elif command == 'names':
-                for i in range(len(city)):
-                    print(airport[i])
-            elif command == '--full':
-                print(data['locations'])
